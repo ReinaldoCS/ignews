30c391623c7f7c71935af584ce82cd7a11ce75ae
Reinaldo Correia <santosdoscorreiareinaldo@gmail.com>
2022-02-03 21:11:10 -0300

    Página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,37 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+import avatar from '../../public/images/avatar.svg';
+
+import styles from './home.module.scss';
+
 export default function Home() {
 
   return (
-    <h1>
-      Hello Worlds
-    </h1>
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+        </section>
+
+        <Image
+          src={avatar}
+          alt="Girl coding"
+        />
+      </main>
+    </>
   )
 }
