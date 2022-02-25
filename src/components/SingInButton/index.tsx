2497f3d556f8a57225ec472dcf6f705cffd15c95
Reinaldo Correia <santosdoscorreiareinaldo@gmail.com>
2022-02-25 12:06:53 -0300

    Autenticação com Next Auth

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,24 +1,28 @@
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
+import { useSession, signIn, signOut } from 'next-auth/react';
+
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
 
 export function SingInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
   
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       type="button"
       className={styles.singInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04D361" />
-      Reinaldo Correia
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.singInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#EBA417" />
       Sing In with GitHub
